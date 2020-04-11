82cf35e9ed1be4784f874f6af7ff660467415c5f
Yi Li <licds@bc.edu>
2020-04-11 18:51:03 -0400

    9.3

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Yi Li on 4/11/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
---
+++ Snacktacular/SpotsListViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by John Gallaugher on 3/23/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+
+
+import UIKit
+import CoreLocation
+import Firebase
+import FirebaseUI
+import GoogleSignIn
+
+class SpotsListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var spots: Spots!
+    var authUI: FUIAuth!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        authUI = FUIAuth.defaultAuthUI()
+        // You need to adopt a FUIAuthDelegate protocol to receive callback
+        authUI?.delegate = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = true
+        
+        spots = Spots()
+        spots.spotArray.append(Spot(name: "El Pelon", address: "Comm. Ave", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+        spots.spotArray.append(Spot(name: "Shake Shack", address: "The street - Chestnut Hill", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+        spots.spotArray.append(Spot(name: "Pino's Pizza", address: "Cleveland Circle", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        signIn()
+    }
+    
+    func signIn() {
+        let providers: [FUIAuthProvider] = [
+            FUIGoogleAuth(),
+        ]
+//        let currentUser = authUI.auth?.currentUser
+        if authUI.auth?.currentUser == nil {
+            self.authUI?.providers = providers
+            let loginViewController = authUI.authViewController()
+            loginViewController.modalPresentationStyle = .fullScreen
+            present(authUI.authViewController(), animated: true, completion: nil)
+        } else {
+            tableView.isHidden = false
+//
+//            snackUser = SnackUser(user: currentUser!)
+//            snackUser.saveIfNewUser()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSpot" {
+            let destination = segue.destination as! SpotDetailViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.spot = spots.spotArray[selectedIndexPath.row]
+        } else {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedIndexPath, animated: true)
+            }
+        }
+    }
+    
+    @IBAction func signOut(_ sender: UIBarButtonItem) {
+        do {
+            try authUI!.signOut()
+            print("^^^ Successfully Sign Out!")
+            tableView.isHidden = true
+            signIn()
+        } catch {
+            tableView.isHidden = true
+            print("*** ERROR: Couldn't Sign Out.")
+        }
+    }
+    
+}
+
+extension SpotsListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spots.spotArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotsTableViewCell
+        cell.nameLabel.text = spots.spotArray[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+}
+
+extension SpotsListViewController: FUIAuthDelegate {
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        if let user = user {
+            tableView.isHidden = false
+            print("*** we signed in with the user \(user.email ?? "Unknown email")")
+        }
+    }
+    
+    func authPickerViewController(forAuthUI authUI: FUIAuth) -> FUIAuthPickerViewController {
+        let loginViewController = FUIAuthPickerViewController(authUI: authUI)
+        loginViewController.view.backgroundColor = UIColor.white
+        
+        let marginInsets: CGFloat = 16
+        let imageHeight: CGFloat = 225
+        let imageY = self.view.center.y - imageHeight
+        let logoFrame = CGRect(x: self.view.frame.origin.x + marginInsets, y: imageY, width: self.view.frame.width - (marginInsets*2), height: imageHeight)
+        let logoImageView = UIImageView(frame: logoFrame)
+        logoImageView.image = UIImage(named: "logo")
+        logoImageView.contentMode = .scaleAspectFit
+        loginViewController.view.addSubview(logoImageView)
+        return loginViewController
+    }
+}
